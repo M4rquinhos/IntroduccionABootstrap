21e19eac472ceff24ca670eb6f99ab66b4cc74bc
Marco Carrillo <marco.37099p@gmail.com>
2023-02-22 15:43:48 -0600

    Agregar archivos de proyecto.

---
+++ IntroduccionABootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroduccionABootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
